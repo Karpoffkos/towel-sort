7b214afc97620e5b1ab4749b015e2bf26eda6fd5
Konstantsin Karpov <karpoffkos@mal.ru>
2023-02-14 15:24:28 +0300

    int: solving

--- src/index.js
+++ src/index.js
@@ -2,5 +2,27 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+ // console.log(matrix, 5)
+ let arr = [];
+ console.log(arguments.length, 7);
+ if(arguments.length == 0){ 
+    return [];
+  }  
+  else {
+    
+    if(matrix.length !== 0){
+      for (let i = 1 ; i <= matrix.length; i++){
+        let pos = i-1;
+        if(i%2 == 0){
+          matrix[pos].reverse().forEach(j => arr.push(j));
+        } else {
+          matrix[pos].forEach(j => arr.push(j));
+        }
+      }
+    // console.log(arr, 16)
+      
+    }
+    console.log(arr, 26)
+    return arr;
+  } 
 }
